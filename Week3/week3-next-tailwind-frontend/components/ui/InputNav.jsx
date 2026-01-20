b62e39c4fabba0a0de6316ebe8769b3e005f91cd
devansh-hestabit <devansh.hestabit@gmail.com>
2026-01-20 13:18:31 +0530

    Added Input for Navbar Search Section

---
+++ Week3/week3-next-tailwind-frontend/components/ui/InputNav.jsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function InputNav({ placeholder }) {
+  return (
+    <input
+      placeholder={placeholder}
+      className="pl-9 pr-3 py-1 rounded text-black bg-white text-sm w-full"
+    />
+  );
+}
--- Week3/week3-next-tailwind-frontend/components/ui/Navbar.jsx
+++ Week3/week3-next-tailwind-frontend/components/ui/Navbar.jsx
@@ -1,9 +1,9 @@
 "use client";
+import Input from "@/components/ui/InputNav";
 
 export default function Navbar({ isOpen, onToggleSidebar }) {
   return (
     <header className="h-16 bg-gray-800 text-white flex items-center justify-between px-6">
-      
       {/* Left side */}
       <div className="flex items-center gap-4">
         <span className="text-lg font-semibold">HestaCart</span>
@@ -27,11 +27,7 @@ export default function Navbar({ isOpen, onToggleSidebar }) {
             🔍
           </span>
 
-          <input
-            type="text"
-            placeholder="Search for..."
-            className="pl-9 pr-3 py-1 rounded text-black bg-white text-sm"
-          />
+          <Input placeholder="Search for..." />
         </div>
 
         <span className="cursor-pointer">🔔</span>
