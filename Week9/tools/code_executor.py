3256504a22f156403076d352afb756f321a179ea
devansh-hestabit <devansh.hestabit@gmail.com>
2026-03-23 12:52:02 +0530

    Logs and Running commands updated

--- Week9/tools/code_executor.py
+++ Week9/tools/code_executor.py
@@ -2,6 +2,18 @@ import io
 import sys
 import traceback
 
+def clean_code(code: str):
+    # remove markdown fences
+    code = code.strip()
+
+    if code.startswith("```"):
+        code = code.split("```")[1]
+
+    # remove possible language tag
+    code = code.replace("python", "", 1)
+
+    return code.strip()
+
 class CodeExecutor:
     def __init__(self):
         self.exec_globals = {}
@@ -11,6 +23,7 @@ class CodeExecutor:
         old_stdout = sys.stdout
         sys.stdout = buffer = io.StringIO()
         try:
+            code = clean_code(code)
             exec(code, self.exec_globals)
             output = buffer.getvalue()
             if output.strip() == "":
